6ce75ba18bd1a4920f1e2be4f5ebd50340c33bd4
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-12-20 21:01:12 +0600

    add to redux slice

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,17 @@
+// Need to use the React-specific entry point to import createApi
+
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+import { axiosBaseQuery } from "../../axios/axiosBaseQuery";
+import { URL } from "../../constant/common";
+
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: axiosBaseQuery({ baseUrl: URL }),
+  endpoints: () => ({}),
+  tagTypes: ["user", "team"],
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
